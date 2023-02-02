5f6e3fe481195c8389cbb4e1f1ac9e80ad5caf83
Veloya <archaic.archea@gmail.com>
2023-02-01 20:15:55 -0500

    using mmio now :P

--- src/lib.rs
+++ src/lib.rs
@@ -4,44 +4,50 @@
 
 use log::{log, Level};
 
-pub static mut FDT: *const u8 = core::ptr::null();
+static mut MMIO: mmio::VolBox<u32, mmio::Deny, mmio::Allow> = unsafe {mmio::VolBox::new(core::ptr::null_mut())};
+static mut OFF: u32 = 0;
+static mut REBOOT: u32 = 0;
 
-pub fn init(fdt: *const u8) {
+pub fn init(fdt_ptr: *const u8) ->  Option<()> {
     unsafe {
-        FDT = fdt;
+        let fdt = fdt::Fdt::from_ptr(fdt_ptr).expect("Invalid Fdt pointer");
+
+        let node = fdt.find_compatible(&["syscon-poweroff"])?;
+        let offset = node.property("offset")?.as_usize()?;
+        OFF = node.property("value")?.as_usize()? as u32;
+
+        let node = fdt.find_compatible(&["syscon-reboot"])?;
+        let offset = node.property("offset")?.as_usize()?;
+        REBOOT = node.property("value")?.as_usize()? as u32;
+
+        let syscon_phandle = node.property("regmap")?.as_usize()? as u32;
+        let syscon_node = fdt.find_phandle(syscon_phandle)?;
+        let syscon_mmio = syscon_node.reg()?.next()?.starting_address.cast::<u32>().cast_mut();
+
+        let mmio: mmio::VolBox<u32, mmio::Deny, mmio::Allow> = mmio::VolBox::new(syscon_mmio);
+
+        MMIO = mmio;
     }
+
+    Some(())
 }
 
-pub fn power_off() ->  Option<!> {
-    let fdt = unsafe {fdt::Fdt::from_ptr(FDT).unwrap()};
-    
-    let poweroff_node = fdt.find_compatible(&["syscon-poweroff"])?;
-    let offset = poweroff_node.property("offset")?.as_usize()?;
-    let value = poweroff_node.property("value")?.as_usize()? as u32;
-    let syscon_phandle = poweroff_node.property("regmap")?.as_usize()? as u32;
-    let syscon_node = fdt.find_phandle(syscon_phandle)?;
-    let syscon_mmio = syscon_node.reg()?.next()?.starting_address.cast::<u32>().cast_mut();
-    log!(Level::Info, "Powering off");
+pub fn power_off() -> ! {
+    log!(Level::Info, "Power off requested");
+
     unsafe {
-        syscon_mmio.byte_add(offset).write_volatile(value);
+        MMIO.write(OFF);
     }
 
-    unreachable!()
+    unreachable!("ERROR OCCURED WHILE POWERING OFF");
 }
 
-pub fn reboot() ->  Option<!> {
-    let fdt = unsafe {fdt::Fdt::from_ptr(FDT).unwrap()};
+pub fn reboot() -> ! {
+    log!(Level::Info, "Reboot requested");
     
-    let reboot = fdt.find_compatible(&["syscon-reboot"])?;
-    let offset = reboot.property("offset")?.as_usize()?;
-    let value = reboot.property("value")?.as_usize()? as u32;
-    let syscon_phandle = reboot.property("regmap")?.as_usize()? as u32;
-    let syscon_node = fdt.find_phandle(syscon_phandle)?;
-    let syscon_mmio = syscon_node.reg()?.next()?.starting_address.cast::<u32>().cast_mut();
-    log!(Level::Info, "Rebooting...");
     unsafe {
-        syscon_mmio.byte_add(offset).write_volatile(value);
+        MMIO.write(REBOOT);
     }
 
-    unreachable!()
+    unreachable!("ERROR OCCURED WHILE REBOOTING");
 }
